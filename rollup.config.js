86270f719c0ab9eba7cf9896a3a320935c0560d8
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2018-05-22 20:37:51 +0200

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 
 export default [
     {

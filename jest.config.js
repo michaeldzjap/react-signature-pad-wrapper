3b1a0910b72f90226e72022fed90a263b9bb9de9
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2020-05-21 18:40:49 +0200

    Move jest config to separate file

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    setupFiles: [
+        '<rootDir>/__tests__/shim.js',
+        '<rootDir>/__tests__/config.js',
+    ],
+    testPathIgnorePatterns: [
+        '<rootDir>/__tests__/shim.js',
+        '<rootDir>/__tests__/config.js',
+        '<rootDir>/__tests__/helpers/',
+        '<rootDir>/node_modules/',
+    ],
+    collectCoverage: true,
+};

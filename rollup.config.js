c2c4182ca77e3dff15246208a0e617cb9491b2a2
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2021-09-17 09:16:35 +0200

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { eslint } from 'rollup-plugin-eslint';
+import eslint from '@rollup/plugin-eslint';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 

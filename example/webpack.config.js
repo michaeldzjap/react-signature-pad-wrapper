a03efcbefb99d1d39ba34e97a76792efbe1cb8e6
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2021-09-26 10:28:39 +0200

    Update dependencies + update webpack config for example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,11 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: {
-        bundle: './app',
         vendor: ['react', 'react-dom'],
+        bundle: {
+            import: './app',
+            dependOn: 'vendor',
+        },
     },
     output: {
         filename: '[name].js',
@@ -25,20 +28,4 @@ module.exports = {
         extensions: ['.js', '.ts', '.tsx'],
     },
     devtool: 'eval-cheap-module-source-map',
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: 'initial',
-                    name: 'vendor',
-                    test: 'vendor',
-                    enforce: true,
-                },
-            },
-        },
-        runtimeChunk: {
-            name: 'manifest',
-        },
-        emitOnErrors: false,
-    },
 };

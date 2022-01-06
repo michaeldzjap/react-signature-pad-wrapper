4525f907cb820861809d3d404b0283cd447a0fe2
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2022-01-06 11:05:50 +0100

    #37: Only include source maps in dev build

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,17 @@ import commonjs from '@rollup/plugin-commonjs';
 // import eslint from '@rollup/plugin-eslint';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import sourceMaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
+const local = process.env.NODE_ENV === 'local';
+
 export default {
     input: 'src/index.ts',
     output: [
-        { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'named' },
-        { file: pkg.module, format: 'es', sourcemap: true },
+        { file: pkg.main, format: 'cjs', sourcemap: local, exports: 'named' },
+        { file: pkg.module, format: 'es', sourcemap: local },
     ],
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     watch: {
@@ -21,10 +22,10 @@ export default {
         // eslint(),
         typescript({
             typescript: require('typescript'),
+            sourceMap: local,
         }),
         commonjs(),
         nodeResolve(),
-        sourceMaps(),
         terser(),
     ],
 };
